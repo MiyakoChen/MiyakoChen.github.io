4828c144956f9a4d0a3f02147c432a7c1244d6c7
chenjie <chenjie@succez.com>
2019-01-18 15:52:11 +0800

    显示时间

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-debugger;
+requestAnimationFrame(() => {
+    let app = new Vue({
+        el: '#app',
+        data: {
+            message: 'the first vue'
+        }
+    })
+    let app2 = new Vue({
+        el: '#app-2',
+        data: {
+            message: '现在时间:' + new Date().toLocaleString()
+        }
+    })
+    let app4 = new Vue({
+        el: '#app-4',
+        data: {
+            todos: [{
+                    text: '第一步'
+                },
+                {
+                    text: '第二步'
+                },
+                {
+                    text: '最后'
+                }
+            ]
+        }
+    })
+    let timeTitle = document.getElementById('app-2');
+    timeTitle.onmousemove = () => {
+        app2.data = '现在时间:' + new Date().toLocaleString();
+    };
+})
