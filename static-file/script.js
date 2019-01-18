8f0854892d80214071a89049da65431c9144a090
chenjie <chenjie@succez.com>
2019-01-18 18:02:33 +0800

    临时提交

--- static-file/script.js
+++ static-file/script.js
@@ -1,33 +1,23 @@
-requestAnimationFrame(() => {
-    let app = new Vue({
-        el: '#app',
-        data: {
-            message: 'the first vue'
-        }
-    })
-    let app2 = new Vue({
-        el: '#app-2',
-        data: {
-            message: '现在时间:' + new Date().toLocaleString()
-        }
-    })
-    let app4 = new Vue({
-        el: '#app-4',
-        data: {
-            todos: [{
-                    text: '第一步'
-                },
-                {
-                    text: '第二步'
-                },
-                {
-                    text: '最后'
-                }
-            ]
-        }
-    })
-    let timeTitle = document.getElementById('app-2');
-    timeTitle.onmousemove = () => {
-        app2.message = '现在时间:' + new Date().toLocaleString();
-    };
-})
+requestAnimationFrame(() => {})
+
+/**
+ * 返回符合的dom，
+ * @param {} id 
+ * @param {*} className 
+ * @param {*} tag 
+ * @returns{htmlelement}
+ */
+function query(id, className, tag) {
+	let els = [];
+	if (className) {
+		els = document.getElementsByClassName(className);
+	}
+	if (tag) {
+		if (els && els.length) {
+			els.filter(el=>el.tagName===tag);
+		} else {
+
+		}
+	}
+	document.getElementsByClassName().find(el => el)
+}
