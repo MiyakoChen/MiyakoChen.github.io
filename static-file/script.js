69e835b59d60369c86f7ffaf01897346df93e36c
chenjie <chenjie@succez.com>
2019-01-19 10:26:18 +0800

    临时提交

--- static-file/script.js
+++ static-file/script.js
@@ -1,11 +1,13 @@
-requestAnimationFrame(() => {})
+requestAnimationFrame(() => {
+	
+})
 
 /**
- * 返回符合的dom，
- * @param {} id 
- * @param {*} className 
- * @param {*} tag 
- * @returns{htmlelement}
+ * 返回符合的dom，如果有多个，只返回符合条件的第一个dom
+ * @param {string} id 
+ * @param {string} className 
+ * @param {string} tag 
+ * @returns {HTMLElement}
  */
 function query(id, className, tag) {
 	let els = [];
@@ -14,10 +16,55 @@ function query(id, className, tag) {
 	}
 	if (tag) {
 		if (els && els.length) {
-			els.filter(el=>el.tagName===tag);
+			els.filter(el => tag.equalsIgnoreCase(el.tagName));
 		} else {
-
+			els = document.getElementsByTagName(tag);
 		}
 	}
-	document.getElementsByClassName().find(el => el)
+	if (id) {
+		if (els && els.length) {
+			els.filter(el => id.equalsIgnoreCase(el.id));
+		} else {
+			els = [document.getElementById(id)];
+		}
+	}
+	return els[0];
+}
+
+String.prototype.equalsIgnoreCase = function (s) {
+	if (s == null)
+		return false;
+	let t = this.toString();
+	let len = t.length;
+	if (len !== s.length) {
+		return false;
+	}
+	if (t === s) {
+		return true;
+	}
+	let c1, c2;
+	for (let i = 0; i < len; i++) {
+		c1 = t.charCodeAt(i);
+		c2 = s.charCodeAt(i);
+		if (c1 !== c2) {
+			if (c1 >= 65 // A
+				&&
+				c1 <= 90 // Z
+			) {
+				if (c1 !== c2 - 32) {
+					return false;
+				}
+			} else if (c1 >= 97 // a
+				&&
+				c2 <= 122 // z
+			) {
+				if (c1 !== c2 + 32) {
+					return false;
+				}
+			} else {
+				return false;
+			}
+		}
+	}
+	return true;
 }
